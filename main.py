00694ff524e72c353ece90e146444bba8f2fde9b
Gguigre <gui.gremi@gmail.com>
2018-10-09 00:15:07 +0200

    the minutes should be less than 60, not less than 24

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def is_args_valid(args):
         sys.exit(-1);
 
     hour = args.hour.split(':', 1)
-    if (int(hour[0]) > 0 and int(hour[0]) < 24 and int(hour[1]) > 0 and int(hour[1]) < 24):
+    if (int(hour[0]) > 0 and int(hour[0]) < 24 and int(hour[1]) > 0 and int(hour[1]) < 60):
         return hour
     print ("\033[31mHour bad formatted\033[0m")
     sys.exit(-1)
